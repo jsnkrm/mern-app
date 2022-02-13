4e1544c09db45339b05daf9cc6c078f1ed06c22b
jsnkrm <m.jsnkr@gmail.com>
2022-02-13 16:51:38 +0530

    Initial REST API for goals

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
+const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 const {errorHandler} = require('./middleware/errorMiddleware')
 
-const app = express()
+connectDB()
 
+const app = express()
+ 
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 
