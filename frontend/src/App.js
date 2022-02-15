e1f6a83b5882d7f67092c4e4a84fdc6752f250fe
jsnkrm <m.jsnkr@gmail.com>
2022-02-16 00:47:13 +0530

    Added auth login logout on frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import Dashboard from './pages/Dashboard'
 import Login from  './pages/Login'
 import Register from './pages/Register'
 import Header from './components/Header'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import {register, reset} from './features/auth/authSlice'
 
 function App() {
   return (
@@ -17,6 +20,7 @@ function App() {
         </Routes> 
       </div>
       </Router>
+      <ToastContainer />
     </>
   );
 }
